17b08ad02d3c08bdb9d3ec37b83ee61acbef9fb3
Molina-Q <quentinmolina1@gmail.com>
2024-07-03 15:57:50 +0200

    Created result component

---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+    <p class="score">
+        Reaction Time:
+        {{ this.score }} ms
+    </p>
+
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ["score"],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = "Snail pace..."
+        }
+    }
+}
+</script>
+
+<style>
+.score {
+    color: cornflowerblue;
+}
+
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
