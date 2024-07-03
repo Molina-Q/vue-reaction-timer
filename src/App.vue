9c2e0e20473b82d0c40bbc0953ef491d7c347f3a
Molina-Q <quentinmolina1@gmail.com>
2024-07-03 15:57:13 +0200

    Added results component and some css

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <h1>Kant1 React Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Results v-if="showResults" :score="score" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
 </template>
 
 <script>
 
 import Block from './components/Block.vue'
+import Results from './components/Results.vue';
 
 export default {
   name: 'App',
   components: {
-    Block
+    Block, Results
   },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResults = false
       // console.log(this.delay);
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 }
@@ -37,5 +47,22 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
